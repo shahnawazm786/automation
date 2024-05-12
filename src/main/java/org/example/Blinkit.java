73bc9b5267032cab961010fe8060c66eaa2c36c7
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-12 09:40:31 +0530

    today class

---
+++ src/main/java/org/example/Blinkit.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Blinkit {
+    public static void main(String[] args) {
+        int a=100;
+        System.out.println(a);
+        System.out.println("Value of a is ");
+        System.out.println(a);
+        System.out.println("Value of a is  " + a); // concatination
+        System.out.println("Value of a is \t" + a);
+        System.out.println("Value of a is \n " + a);
+    }
+}
