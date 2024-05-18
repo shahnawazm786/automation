c0f039b49c5e23f171eb9280448740994e7e80ef
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-18 11:52:46 +0530

    today class

---
+++ src/main/java/org/example/day02/Session_02_01.java
@@ -0,0 +1,18 @@
+package org.example.day02;
+
+public class Session_02_01 {
+    public static void main(String[] args) {
+        // conditional operator (true, false)
+        // > < >= <= == !=
+        System.out.println(100>50); // true
+        System.out.println(100<50); // false
+        System.out.println(100>=50); // true
+        System.out.println(100<=50); // false
+        System.out.println(100==100); // true
+        System.out.println(100>=100); // true
+        System.out.println(100<=100); //true
+        System.out.println(100!=100); // false
+        System.out.println(100!=50); // true
+        System.out.println(50!=500); // true
+    }
+}
