798266fe6851d821bcd825cb1318be0cd9642be6
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-18 11:53:16 +0530

    today class

---
+++ src/main/java/org/example/day02/Session_02_05.java
@@ -0,0 +1,25 @@
+package org.example.day02;
+
+public class Session_02_05 {
+    public static void main(String[] args) {
+        // post opetrator ++
+        // ++ pre operator
+        int x=100;
+        System.out.println(x);//100
+        // post operator
+        int y=x++; // assigment then increment
+        System.out.println(x);//101
+        System.out.println(y);//100
+        // Pre increment
+        int z=100;
+        System.out.println(z);
+        int a=++z;
+        System.out.println(z);
+        System.out.println(a);
+        int b=++z;z++;
+        System.out.println(b);
+        System.out.println(z);
+        int l=1;
+
+    }
+}
