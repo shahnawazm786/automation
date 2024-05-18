facd84620f638cc6598fd6737e851240daae447b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-18 11:52:37 +0530

    today class

---
+++ src/main/java/org/example/day02/Session_01_02.java
@@ -0,0 +1,17 @@
+package org.example.day02;
+
+public class Session_01_02 {
+    public static void main(String[] args) {
+        // Basic operator
+        // + - * / %
+
+        System.out.println(100+200); //300
+        System.out.println(500-200); // 300
+        System.out.println(30*4); // 120
+        System.out.println(50/7);//7
+        System.out.println(50/7.0);//7
+        System.out.println(50%7);
+        System.out.println(50%9);
+
+    }
+}
