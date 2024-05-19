4af0ca7bebef2b4a74e170c5aa9f517f28a24915
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-19 09:58:44 +0530

    automation class

---
+++ src/main/java/org/example/day03/Session_01_01.java
@@ -0,0 +1,26 @@
+package org.example.day03;
+
+public class Session_01_01 {
+    public static void main(String[] args) {
+        // assignment operator
+        //  =, +=, -=, *=, /=, %=
+        int a=100; //==
+        a+=10; // a= a+10 // 110
+        System.out.println(a);
+        a-=100; // a= a-100 // 10
+        System.out.println(a);
+        a*=5; // a=a*5 // 50
+        System.out.println(a);
+        a/=7; // a=a/7 =>7
+        System.out.println(a);
+        a%=10; //  a=a%10 => a= 1%10 => 7
+        System.out.println(a);
+        int b= 2+a;
+        System.out.println(b);
+        int c=a+2;
+        System.out.println(c);
+        // = unary opeator
+        // arithmetic operator, conditional, logical, assignment operator - binary
+        // ()? expre : expr
+    }
+}
