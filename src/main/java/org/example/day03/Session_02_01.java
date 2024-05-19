dd372fc1fd041f7e23f5dbf604bf6b7cd8536612
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-05-19 09:59:12 +0530

    if

---
+++ src/main/java/org/example/day03/Session_02_01.java
@@ -0,0 +1,63 @@
+package org.example.day03;
+
+import java.util.Scanner;
+
+public class Session_02_01 {
+    public static void main(String[] args) {
+        double salary;
+        double tax;
+        double net_sal;
+        int ans;
+        // salary >50000 - 10%
+        // salary >45000 <50000 - 8%
+        // salary >35000 <45000 - 6%
+        // >25000 <350000 - 4%
+        // <25000 - no tax
+
+        // if -  else if - else
+        do {
+            System.out.println("Enter your salary for tax calculation");
+            Scanner scanner = new Scanner(System.in);
+            salary = scanner.nextDouble();
+            System.out.println("Your salary is \t" + salary);
+            /*Tax calculator*/
+            if (salary >= 50000) {
+
+                tax = (salary * 10) / 100;
+                System.out.println("Your tax to be paid \t" + tax);
+                net_sal = salary - tax;
+                System.out.println("Your Net Salary is \t" + net_sal);
+            } else if (salary >= 45000 & salary < 50000) {
+
+                tax = (salary * 8) / 100;
+                System.out.println("Your tax to be paid \t" + tax);
+                net_sal = salary - tax;
+                System.out.println("Your Net Salary is \t" + net_sal);
+            } else if (salary >= 35000 & salary < 45000) {
+
+                tax = (salary * 6) / 100;
+                System.out.println("Your tax to be paid \t" + tax);
+                net_sal = salary - tax;
+                System.out.println("Your Net Salary is \t" + net_sal);
+            } else if (salary >= 25000 & salary < 35000) {
+
+                tax = (salary * 4) / 100;
+                System.out.println("Your tax to be paid \t" + tax);
+                net_sal = salary - tax;
+                System.out.println("Your Net Salary is \t" + net_sal);
+            } else {
+                tax = 0;
+                System.out.println("Your tax to be paid \t" + tax);
+                net_sal = salary - tax;
+                System.out.println("Your Net Salary is \t" + net_sal);
+
+            }
+            System.out.println("==========================================");
+            System.out.println("Do you want to continue ..... ");
+            System.out.println("Press 1 to continue");
+            System.out.println("Press 0 to exit ");
+            ans=scanner.nextInt();
+            System.out.println("==========================================");
+        }while (ans==1);
+    }
+}
