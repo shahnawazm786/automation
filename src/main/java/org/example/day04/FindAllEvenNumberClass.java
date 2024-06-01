1eb919f6ac5465343f909bff3597aa7bb66869cd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-01 10:42:28 +0530

    Loop

--- src/main/java/org/example/day04/FindAllEvenNumberClass.java
+++ src/main/java/org/example/day04/FindAllEvenNumberClass.java
@@ -1,7 +1,18 @@
 package org.example.day04;
 
 public class FindAllEvenNumberClass {
+    // find all the even number between 1 and 100.
+    // a number divided by 2 and has no reminder
+    // % reminder operator
+    // number%2==0
     public static void main(String[] args) {
-
+        int number=1;
+        while (number<=100){
+            //System.out.print(number+"\t");
+            if(number%2==0){
+                System.out.print(number+"\t");
+            }
+            number++;
+        }
     }
 }
