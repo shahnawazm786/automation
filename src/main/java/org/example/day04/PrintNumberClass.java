21efe7f97a259c56f0f03eb279c80972b7bdf8ed
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-01 10:38:16 +0530

    Loop

---
+++ src/main/java/org/example/day04/PrintNumberClass.java
@@ -0,0 +1,18 @@
+package org.example.day04;
+
+public class PrintNumberClass {
+    public static void main(String[] args) {
+        // write a program to print 1-100 number
+        // start 1
+        // end 100
+        // step ?
+
+        int ctr=1;
+        while(ctr<=100){
+            System.out.print(ctr+"\t");
+            //ctr++;
+            ctr=ctr+1;
+        }
+
+    }
+}
