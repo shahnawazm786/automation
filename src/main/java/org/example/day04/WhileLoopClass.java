992adeccd64e741d8d0973a79411490ea7606927
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-01 10:06:19 +0530

    Ternary

---
+++ src/main/java/org/example/day04/WhileLoopClass.java
@@ -0,0 +1,7 @@
+package org.example.day04;
+
+public class WhileLoopClass {
+    // Loop - while, do-while, for,foreach or for object
+    // loop is used when the user require to execute particular line of code / block for certain time.
+
+}
