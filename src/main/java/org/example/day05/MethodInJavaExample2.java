94bd73b5d8e3a6f863c7779b5c9dc712995a0db1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-02 10:51:53 +0530

    Loop

---
+++ src/main/java/org/example/day05/MethodInJavaExample2.java
@@ -0,0 +1,10 @@
+package org.example.day05;
+
+public class MethodInJavaExample2 {
+    void Add(){
+        System.out.println("Add Method");
+    }
+    static void Subs(){
+        System.out.println("Static method substract");
+    }
+}
