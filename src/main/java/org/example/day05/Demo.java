3b1cb82efd42359bc4b83687753d5617d0cbf86d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-02 10:51:26 +0530

    Loop

---
+++ src/main/java/org/example/day05/Demo.java
@@ -0,0 +1,11 @@
+package org.example.day05;
+
+//import static org.example.day05.MethodInJavaExample2.Subs;
+
+public class Demo {
+    public static void main(String[] args) {
+        MethodInJavaExample2.Subs();
+        MethodInJavaExample2 obj=new MethodInJavaExample2();
+        obj.Add();
+    }
+}
