f657afeb2ea20fbce69a3fad52871c832df182b9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 10:37:13 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodIndexOfExample.java
@@ -0,0 +1,9 @@
+package org.example.day06;
+
+public class StringMethodIndexOfExample {
+    public static void main(String[] args) {
+        String str="Java is a programming language";
+        System.out.println(str.indexOf("p"));
+        System.out.println(str.indexOf("a"));
+    }
+}
