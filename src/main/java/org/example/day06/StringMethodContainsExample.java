3fc9db82b353fffed7c821944f27e7262fa0c411
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 09:57:40 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodContainsExample.java
@@ -0,0 +1,11 @@
+package org.example.day06;
+
+public class StringMethodContainsExample {
+    public static void main(String[] args) {
+        String str="Java is high level language";
+        System.out.println(str.contains("high"));//true
+        System.out.println(str.contains("High"));//false
+
+
+    }
+}
