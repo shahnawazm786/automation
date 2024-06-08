d1b9e26c8a7cd51fa403c9a11599291bcf3432e0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 10:37:40 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodLowerAndUpperExample.java
@@ -0,0 +1,8 @@
+package org.example.day06;
+
+public class StringMethodLowerAndUpperExample {
+    public static void main(String[] args) {
+        System.out.println("java".toUpperCase()); //JAVA
+        System.out.println("JAVA".toLowerCase()); //JAVA
+    }
+}
