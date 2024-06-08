05afc59add9ffc8cc13a4f883068f4ea30ba43c3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 09:57:34 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodConcatExample.java
@@ -0,0 +1,15 @@
+package org.example.day06;
+
+public class StringMethodConcatExample {
+    public static void main(String[] args) {
+        String exp="Java";
+        String exp1=" Language";
+        System.out.println(exp.concat(exp1));
+        // JaVA
+        // langUAGe
+        // JVAUAG
+        String result=exp + exp1;
+        System.out.println(result);
+
+    }
+}
