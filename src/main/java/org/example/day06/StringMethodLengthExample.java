ab8778993cba5fe5aa6f583badce31d2339ded4c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 09:57:53 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodLengthExample.java
@@ -0,0 +1,10 @@
+package org.example.day06;
+
+public class StringMethodLengthExample {
+    public static void main(String[] args) {
+        String expr="Java";
+        System.out.println("Size of string is =>"+expr.length());
+        expr="java is high level program";
+        System.out.println("Size of string is =>"+expr.length());
+    }
+}
