4e7e24812de1d7a53c9c564bb90780652236de04
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 09:57:30 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodCharAtExample.java
@@ -0,0 +1,18 @@
+package org.example.day06;
+
+public class StringMethodCharAtExample {
+    public static void main(String[] args) {
+        String str="Hello Java";
+        System.out.println(str);
+        // H - 0
+        // e - 1
+        // l - 2
+        System.out.println(str.charAt(0)); // H
+        System.out.println(str.charAt(3)); // l
+        System.out.println(str.charAt(6)); // J
+        // vowels - filter
+        // vowels - A / a - count
+        // count the word
+
+    }
+}
