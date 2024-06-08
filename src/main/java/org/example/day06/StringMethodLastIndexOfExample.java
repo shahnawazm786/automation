c0b0b92dec5c0125628e8ae81b3873a4fe441bc3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-08 10:37:33 +0530

    Loop

---
+++ src/main/java/org/example/day06/StringMethodLastIndexOfExample.java
@@ -0,0 +1,9 @@
+package org.example.day06;
+
+public class StringMethodLastIndexOfExample {
+    public static void main(String[] args) {
+        String str="java is programmign language";
+        System.out.println(str.lastIndexOf("a"));
+        System.out.println("Oaracle".lastIndexOf('a'));
+    }
+}
