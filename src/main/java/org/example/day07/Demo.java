018f438e00beb590caca7499a2f49807e60786e9
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-09 11:17:12 +0530

    String Method

---
+++ src/main/java/org/example/day07/Demo.java
@@ -0,0 +1,9 @@
+package org.example.day07;
+
+public class Demo {
+    public static void main(String[] args) {
+        double sal=50000;
+        double sal_cal=sal* (FinalStaticKeywordWithVariableExample.tax_cal_employee)/100;
+        System.out.println(sal_cal);
+    }
+}
