02fee2f9434322eaa57d1ac34eea1e71c2eeadd5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-15 09:46:24 +0530

    Inheritance

---
+++ src/main/java/org/example/java08/singleinheritance/SingleInhtiranceAccess.java
@@ -0,0 +1,9 @@
+package org.example.java08.singleinheritance;
+
+public class SingleInhtiranceAccess {
+    public static void main(String[] args) {
+        DogClass d=new DogClass();
+        d.Eat();
+        d.Barks();
+    }
+}
