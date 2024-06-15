8a1b9c98778b4df4035e36213f637a0e5a4fa0ec
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-15 09:46:34 +0530

    Inheritance

---
+++ src/main/java/org/example/java08/singleinheritance/ViechleClass.java
@@ -0,0 +1,11 @@
+package org.example.java08.singleinheritance;
+
+public class ViechleClass {
+    double speeds=40;
+    public void Start(){
+        System.out.println("All Viechle statrs ...............");
+    }
+    public double Speed(){
+        return this.speeds;
+    }
+}
