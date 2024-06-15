86880bb3573c0f016e702307cd8643ac306d60db
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-15 09:46:08 +0530

    Inheritance

---
+++ src/main/java/org/example/java08/multilevel/DemoMultiLevel.java
@@ -0,0 +1,10 @@
+package org.example.java08.multilevel;
+
+public class DemoMultiLevel {
+    public static void main(String[] args) {
+        CalfsClass calfsClass=new CalfsClass();
+        calfsClass.Eat();
+        calfsClass.Eats();
+        calfsClass.DrinkMilk();
+    }
+}
