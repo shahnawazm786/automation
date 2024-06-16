e7bbdf10def2eed02f7189a0995ea87aebe55082
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-16 11:48:30 +0530

    Interface example

---
+++ src/main/java/org/example/day09/polymorphism/ChildClass.java
@@ -0,0 +1,7 @@
+package org.example.day09.polymorphism;
+
+public class ChildClass extends ParentClass{
+    void overriddingMethod(){
+        System.out.println("Method is in child class");
+    }
+}
