8509497b502225c4cedec9a7acef13c816190b34
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-16 11:50:55 +0530

    Interface example

---
+++ src/main/java/org/example/day09/heirarchical/Test.java
@@ -0,0 +1,9 @@
+package org.example.day09.heirarchical;
+
+import org.example.day09.abstraction.IExample;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(IExample.tax);
+    }
+}
