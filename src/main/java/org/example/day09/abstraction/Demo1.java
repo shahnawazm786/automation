884afeabe39d5c38849bfb73e9438eb44560db02
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-16 11:48:46 +0530

    Interface example

---
+++ src/main/java/org/example/day09/abstraction/Demo1.java
@@ -0,0 +1,8 @@
+package org.example.day09.abstraction;
+
+public class Demo1 implements IExampl1{
+    @Override
+    public void tax_cal() {
+        System.out.println("Tax calculation");
+    }
+}
