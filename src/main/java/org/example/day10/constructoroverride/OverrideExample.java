7daa12ac9b7b1a9bb07fd5cf7c26e9869bc0bbea
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:47:02 +0530

    Interface example

---
+++ src/main/java/org/example/day10/constructoroverride/OverrideExample.java
@@ -0,0 +1,11 @@
+package org.example.day10.constructoroverride;
+
+public class OverrideExample {
+    int range=100;
+    public OverrideExample(){
+        System.out.println("Ovveride constructor");
+    }
+    void learn(){
+        System.out.println("I am learning");
+    }
+}
