8ae05274162d6197c2d44f466819834ddcb47077
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:46:17 +0530

    Interface example

---
+++ src/main/java/org/example/protect/Demo.java
@@ -0,0 +1,9 @@
+package org.example.protect;
+
+public class Demo {
+    public static void main(String[] args) {
+        PackageModiferInDifferentPackage ob=new PackageModiferInDifferentPackage();
+        ob.disp();
+
+    }
+}
