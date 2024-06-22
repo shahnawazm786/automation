bf74659b858bd05d60fc1463cb04eba7c0df2202
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:47:27 +0530

    Interface example

---
+++ src/main/java/advance/PackageModifierExampleInAnotherPackage.java
@@ -0,0 +1,11 @@
+package advance;
+
+import extra.PackageModifierExample;
+
+public class PackageModifierExampleInAnotherPackage extends PackageModifierExample {
+    @Override
+    protected void disp() {
+        super.disp();
+    }
+
+}
