88e7a8d5f50c044b16025d72f8cafafd2d337c59
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:46:00 +0530

    Interface example

---
+++ src/main/java/org/example/day10/constructoroverride/Demo.java
@@ -0,0 +1,8 @@
+package org.example.day10.constructoroverride;
+
+public class Demo {
+    public static void main(String[] args) {
+        //OverrideExample e=new OverrideExample();
+        OverrideExample1 e1=new OverrideExample1();
+    }
+}
