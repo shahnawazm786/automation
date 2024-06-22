0aa5666ed9cb7d3c765a4d66aed35a0fb208de31
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 08:38:25 +0530

    Interface example

---
+++ src/main/java/extra/SubClassVariable.java
@@ -0,0 +1,8 @@
+package extra;
+
+public class SubClassVariable extends ParentClassVariable {
+    int speed=200;
+    void disp(){
+        System.out.println("Speed method in sub class");
+    }
+}
