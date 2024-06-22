68fc58abb5d31d65a1cafc7a0cd84330ba30aeaf
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:46:07 +0530

    Interface example

---
+++ src/main/java/org/example/day10/Demo.java
@@ -0,0 +1,8 @@
+package org.example.day10;
+
+public class Demo {
+    public static void main(String[] args) {
+        PackageModifierInPackage ob=new PackageModifierInPackage();
+        ob.disp();
+    }
+}
