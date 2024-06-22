9771bcfe93b03e599c4faa518e8edd46ffdf78f3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:45:03 +0530

    Interface example

---
+++ src/main/java/org/example/protect/defaultpck/ClassOneExtends.java
@@ -0,0 +1,10 @@
+package org.example.protect.defaultpck;
+
+import org.example.protect.ClassOne;
+
+public class ClassOneExtends extends ClassOne {
+    public static void main(String[] args) {
+        ClassOneExtends c=new ClassOneExtends();
+
+    }
+}
