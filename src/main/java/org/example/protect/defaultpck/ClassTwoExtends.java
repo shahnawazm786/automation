3c23350a7c0512416a6d0f8b46a317250778e837
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:45:12 +0530

    Interface example

---
+++ src/main/java/org/example/protect/defaultpck/ClassTwoExtends.java
@@ -0,0 +1,10 @@
+package org.example.protect.defaultpck;
+
+import org.example.protect.ClassTwo;
+
+public class ClassTwoExtends extends ClassTwo {
+    public static void main(String[] args) {
+        ClassTwoExtends c1=new ClassTwoExtends();
+        c1.disp();
+    }
+}
