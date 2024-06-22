b0e95967b6ef2261c41a5406614fb553f7e1ff0d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:47:48 +0530

    Interface example

---
+++ src/main/java/org/example/day10/variableoverriding/VeriabeOverrideExample.java
@@ -0,0 +1,8 @@
+package org.example.day10.variableoverriding;
+
+public class VeriabeOverrideExample {
+    int gear=4;
+    void speed(){
+        System.out.println("Speed() method in VeriabeOverrideExample");
+    }
+}
