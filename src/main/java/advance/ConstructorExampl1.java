22b0c9ba0ef6bdc0f73557cfa2ee4c94852315df
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:45:40 +0530

    Interface example

---
+++ src/main/java/advance/ConstructorExampl1.java
@@ -0,0 +1,9 @@
+package advance;
+
+public class ConstructorExampl1 extends ConstructorExample{
+
+    public ConstructorExampl1(){
+        //super();
+        System.out.println("Default Constructor in ConstructorExampl1");
+}
+}
