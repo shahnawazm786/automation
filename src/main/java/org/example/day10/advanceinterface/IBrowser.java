6c8407550b970f10118a5a203c5bc53de6526d1e
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-06-22 10:46:58 +0530

    Interface example

---
+++ src/main/java/org/example/day10/advanceinterface/IBrowser.java
@@ -0,0 +1,10 @@
+package org.example.day10.advanceinterface;
+
+public interface IBrowser {
+    void browser();
+    void navigate();
+    void maximize();
+    void minimize();
+    void inspect();
+
+}
