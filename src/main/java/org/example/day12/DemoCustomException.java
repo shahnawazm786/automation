22c95d1b857db39d8e7831c49248539485cd30bd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-07-06 10:42:44 +0530

    exception and excel operations

---
+++ src/main/java/org/example/day12/DemoCustomException.java
@@ -0,0 +1,7 @@
+package org.example.day12;
+
+public class DemoCustomException {
+    public static void main(String[] args) {
+        throw new AgeExceptionCustomeClass("Raised my exception here");
+    }
+}
