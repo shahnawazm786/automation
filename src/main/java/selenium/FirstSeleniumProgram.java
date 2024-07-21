a8f4f3cd585af198b9fcb960e3e786a1c04c1625
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-07-21 21:02:19 +0530

    collection framework

---
+++ src/main/java/selenium/FirstSeleniumProgram.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstSeleniumProgram {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--incognito");
+        //options.addArguments("--window-maximize");
+        System.setProperty("webdriver.chrome.driver","src\\main\\resources\\drivers\\chrome\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://www.selenium.dev/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.manage().window().minimize();
+        Thread.sleep(3000);
+        driver.manage().window().fullscreen();
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
