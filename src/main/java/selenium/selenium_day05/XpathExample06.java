95a09b910cc0c5bc76bb01487416347093aa09b8
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-08-04 10:54:54 +0530

    TODO List

---
+++ src/main/java/selenium/selenium_day05/XpathExample06.java
@@ -0,0 +1,40 @@
+package selenium.selenium_day05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XpathExample06 {
+    static WebDriver driver;
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.get("file:///C:/weekly-classes/automation/src/main/resources/xpath_example/xpath_02.html");
+        Thread.sleep(5000);
+        boolean flag;
+        /*try {
+             flag= driver.findElement(By.xpath("//p[@id='p2']")).isDisplayed();
+        }catch (NoSuchElementException nse){
+            flag=false;
+        }*/
+        List<WebElement> elements=new ArrayList<>();
+        try {
+            elements  = driver.findElements(By.xpath("//label[text()='First Name']"));
+            // if element found then extract the paragraph text
+
+        }catch (NoSuchElementException nse){
+            flag=false;
+        }
+        for(WebElement el:elements){
+            System.out.println("Extracting text of each paragraph "+ el.getText());
+        }
+        driver.quit();
+    }
+}
