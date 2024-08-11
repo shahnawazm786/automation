a3599e9859e101b89a0252120e5cef17d414ba31
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-08-11 11:01:58 +0530

    POM selenium

--- src/main/java/selenium/selenium_day07/BootStrapPage.java
+++ src/main/java/selenium/selenium_day07/BootStrapPage.java
@@ -10,12 +10,18 @@ public class BootStrapPage {
     public BootStrapPage(WebDriver driver){
         this.driver=driver;
     }
-    By dropdownMenuButton=By.id("dropdownMenuButton");
+    By dropdownMenuButton=By.xpath("//button[@id='dropdownMenuButton']");
     By dropOptions=By.xpath("(//a[@class='dropdown-item' and text()='Action'])[1]");
-    public  void clickOnDropDownMenu(){
-        driver.findElement(dropdownMenuButton).click();
+    public  void clickOnDropDownMenu() throws InterruptedException{
+        CommonMethod.scrollTOView(driver,dropdownMenuButton);
+        Thread.sleep(2000);
+        CommonMethod.waitTillElementClickable(driver,dropdownMenuButton,20);
+        //driver.findElement(dropdownMenuButton).click();
+        CommonMethod.clcikJS(driver,dropdownMenuButton);
     }
     public void selectOptions(){
-        driver.findElement(dropOptions).click();
+        CommonMethod.scrollTOView(driver,dropOptions);
+        CommonMethod.clcikJS(driver,dropOptions);
+        //driver.findElement(dropOptions).click();
     }
 }
