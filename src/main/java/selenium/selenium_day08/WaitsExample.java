635e343f7209acedc83d0b92cff6a09d8be2485f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-08-17 10:42:44 +0530

    POM selenium

---
+++ src/main/java/selenium/selenium_day08/WaitsExample.java
@@ -0,0 +1,28 @@
+package selenium.selenium_day08;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class WaitsExample {
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.pocruises.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        try {
+            driver.findElement(By.xpath("//span[text()='Find & Book']")).click();
+        }
+        catch (ElementClickInterceptedException nse){
+            JavascriptExecutor js=(JavascriptExecutor) driver;
+            WebElement ele = driver.findElement(By.xpath("//span[text()='Find & Book']"));
+            js.executeScript("arguments[0].click();",ele);
+        }
+       // driver.quit();
+    }
+}
