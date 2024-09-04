89ce875e70a6197933cc7a229d2c307afdff646d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-09-04 09:31:13 +0530

    wait and iframe selenium

--- src/main/java/browserstack/BrowserStackExample.java
+++ src/main/java/browserstack/BrowserStackExample.java
@@ -12,14 +12,15 @@ public class BrowserStackExample {
     public static void main(String[] args) throws MalformedURLException {
         final String UserName="automationseleni_k7KD4O";
         final String AccessKey="vKYs2WpvYbyby6CYQ7T7";
+        //String Url="http://hub.browserstack.com/wd/hub";
         String Url="https://"+UserName+":"+AccessKey+"@hub-cloud.browserstack.com/wd/hub";
         DesiredCapabilities cap=new DesiredCapabilities();
-        cap.setCapability("browserstack.user",UserName);
-        cap.setCapability("browserstack.key",AccessKey);
-        /*cap.setCapability("os","Windows");
+        /*cap.setCapability("browserstack.user",UserName);
+        cap.setCapability("browserstack.key",AccessKey);*/
+        cap.setCapability("os","Windows");
         cap.setCapability("osVersion","10");
         cap.setCapability("browserName","Chrome");
-        cap.setCapability("browserVersion","120.0");*/
+        cap.setCapability("browserVersion","120.0");
         //WebDriver driver = new ChromeDriver();
         WebDriver driver=new RemoteWebDriver(new URL(Url),cap);
         driver.get("https://www.google.com");
