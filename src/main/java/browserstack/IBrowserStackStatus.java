0e74bfaf55d8e12df0809e8d4bb1ebeebae24ee3
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-09-05 09:03:43 +0530

    wait and iframe selenium

--- src/main/java/browserstack/IBrowserStackStatus.java
+++ src/main/java/browserstack/IBrowserStackStatus.java
@@ -1,6 +1,6 @@
 package browserstack;
 
 public interface IBrowserStackStatus {
-    final String STATUS_PASSED_JS="browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"Status\": \"passed\", \"reason\": \"Title matched!\"}}";
+    final String STATUS_PASSED_JS="browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Title matched!\"}}";
     final String STATUS_FAILED_JS="browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Title not matched!\"}}";
 }
