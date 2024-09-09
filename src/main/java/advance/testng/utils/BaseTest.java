9e7d64e2b89b56a09bd81489e220b4e2f7c5a422
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-09-09 08:11:02 +0530

    testng day 02

--- src/main/java/advance/testng/utils/BaseTest.java
+++ src/main/java/advance/testng/utils/BaseTest.java
@@ -8,34 +8,34 @@ import org.testng.TestListenerAdapter;
 public class BaseTest extends TestListenerAdapter {
     @Override
     public void onStart(ITestContext testContext) {
-        super.onStart(testContext);
+        //super.onStart(testContext);
         System.out.println(testContext.getName() +"\t");
-        System.out.println(testContext +" Test Started");
+        System.out.println(testContext +" Test Started in Base Test class");
         System.out.println(testContext.getHost());
 
     }
 
     @Override
     public void onTestSkipped(ITestResult tr) {
-        super.onTestSkipped(tr);
-        System.out.println(tr+"\t => Test Skipped");
+        //super.onTestSkipped(tr);
+        System.out.println(tr.getName()+"\t => Test Skipped in Base Test class");
     }
 
     @Override
     public void onTestFailure(ITestResult tr) {
         //super.onTestFailure(tr);
-        System.out.println(tr+"\t => Test Failed");
+        System.out.println(tr.getName()+"\t => Test Failed");
     }
 
     @Override
     public void onTestStart(ITestResult result) {
-        super.onTestStart(result);
-        System.out.println(result.getName()+"\tTest started");
+        //super.onTestStart(result);
+        System.out.println(result.getName()+"\tTest started in Base Test class");
     }
 
     @Override
     public void onTestSuccess(ITestResult tr) {
-        super.onTestSuccess(tr);
-        System.out.println(tr.getName()+"\t success");
+        //super.onTestSuccess(tr);
+        System.out.println(tr.getName()+"\t success in Base Test class");
     }
 }
